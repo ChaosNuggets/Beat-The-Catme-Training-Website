1bf7013552840521e495b89887d99d6c1d96ccd5
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2022-10-14 03:15:10 -0400

    work now maybe?

--- display_answers.js
+++
@@ -1,7 +0,0 @@
-const answers = JSON.parse(localStorage.getItem('answers'));
-
-for (let i = 0; i < answers.length; i++) {
-    document.getElementById(`ans${i + 1}`).innerHTML = answers[i];
-}
-
-localStorage.clear();
